55aea737fb064a6bc2563739e1c68e40d22ae5b7
sheza <70382989+sheza18@users.noreply.github.com>
2021-03-17 21:55:14 -0400

    in process

---
+++ scope.js
@@ -0,0 +1,14 @@
+function avg(a, b){ 
+   
+     var result = (a + b)/2;
+     return result;  
+  }
+  
+  var num1 = 5;
+  var num2 = 7;
+  
+   var resultingAverage = avg(num1, num2)
+   console.log("The average is: "+resultingAverage);
+   document.write("The average is: "+resultingAverage);
+  alert("The average is: "+resultingAverage);
+
